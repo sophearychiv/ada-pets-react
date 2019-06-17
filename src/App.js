e940a8bb7720b6af6b4bb853af5e4e91cf5f899f
Sophearychiv <sopheary.chiv@gmail.com>
2019-06-17 11:11:33 -0700

    initial set up

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ class App extends Component {
           { /* Wave 2:  Where Pet Details should appear */ }
         <section className="pet-list-wrapper">
           { /* Wave 1:  Where PetList should appear */ }
+          <PetList/>
         </section>
         <section className="new-pet-form-wrapper">
           { /* Wave 3:  Where NewPetForm should appear */ }
